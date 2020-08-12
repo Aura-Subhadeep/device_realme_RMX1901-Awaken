621ead18b87326a075277ce97b01e70853617f81
Lucchetto <lucchetto.tie@live.com>
2020-08-12 14:14:36 +0200

    RMX1901: Create dummy libqti-perfd-client

--- device.mk
+++ device.mk
@@ -292,6 +292,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.perf.scroll_opt=true
 
+# Perfd (dummy)
+PRODUCT_PACKAGES += \
+    libqti-perfd-client
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml \
