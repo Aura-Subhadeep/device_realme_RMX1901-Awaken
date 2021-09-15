658d8520e832b6cb4b4a0eb98b013f52e74b6044
Sai Kishan <kishansai95@gmail.com>
2021-09-15 09:17:53 +0000

    RMX1901: Drop FM
    Signed-off-by: Sai Kishan <kishansai95@gmail.com>

--- device.mk
+++ device.mk
@@ -175,12 +175,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     vendor/lineage/config/permissions/vendor.lineage.biometrics.fingerprint.inscreen.xml:vendor/etc/permissions/vendor.lineage.biometrics.fingerprint.inscreen.xml
 
-# FM
-PRODUCT_PACKAGES += \
-    FM2 \
-    libqcomfm_jni \
-    qcom.fmradio
-
 # IPA
 PRODUCT_PACKAGES += \
     ipacm \
