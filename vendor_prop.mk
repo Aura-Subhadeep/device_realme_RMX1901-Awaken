9566c35f66d28c7828e398ace2abcbde6ecee301
Sai Kishan <kishansai95@gmail.com>
2021-10-16 16:42:46 +0000

    RMX1901: Disable blurs
    * Our soc can't handle a12 blurs
    
    Signed-off-by: Sai Kishan <kishansai95@gmail.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -91,7 +91,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Blur
 PRODUCT_PRODUCT_PROPERTIES += \
-    ro.surface_flinger.supports_background_blur=1 \
+    ro.surface_flinger.supports_background_blur=0 \
     ro.sf.blurs_are_expensive=1
 
 # Camera
