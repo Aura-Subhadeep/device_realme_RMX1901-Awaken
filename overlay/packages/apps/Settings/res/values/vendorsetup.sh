d872bad3b54e71bfe1ade826f7b849ea45c870e3
Subhadeep354 <myspotsub@gmail.com>
2022-03-29 04:00:14 +0000

    RMX1901: removed duplicate vendor.sh
    This reverts commit 938cf30af41c06db2e1b768f951f6aa0ab272548.

--- overlay/packages/apps/Settings/res/values/vendorsetup.sh
+++
@@ -1,6 +0,0 @@
-rm -rf hardware/qcom-caf/sdm845/audio
-rm -rf hardware/qcom-caf/sdm845/media
-rm -rf hardware/qcom-caf/sdm845/display
-git clone https://github.com/aex-tmp/platform_hardware_qcom_audio.git -b 12.x-caf-sdm845 hardware/qcom-caf/sdm845/audio
-git clone https://github.com/aex-tmp/platform_hardware_qcom_media.git -b 12.x-caf-sdm845 hardware/qcom-caf/sdm845/media
-git clone https://github.com/aex-tmp/platform_hardware_qcom_display.git -b 12.x-caf-sdm845 hardware/qcom-caf/sdm845/display
