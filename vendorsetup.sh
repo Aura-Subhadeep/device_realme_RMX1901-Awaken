e1c638f095896902e76a1f14ba08304c1d60b58d
ashim-anwar <ashim.anwar22.com>
2022-09-05 11:36:43 +0000

    RMX1901: Switched To Proton Clang

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -4,3 +4,4 @@ rm -rf hardware/qcom-caf/sdm845/display
 git clone https://github.com/aex-tmp/platform_hardware_qcom_audio.git -b 12.x-caf-sdm845 hardware/qcom-caf/sdm845/audio
 git clone https://github.com/aex-tmp/platform_hardware_qcom_media.git -b 12.x-caf-sdm845 hardware/qcom-caf/sdm845/media
 git clone https://github.com/aex-tmp/platform_hardware_qcom_display.git -b 12.x-caf-sdm845 hardware/qcom-caf/sdm845/display
+git clone --depth=1 https://github.com/kdrag0n/proton-clang.git prebuilts/clang/host/linux-x86/clang-proton
