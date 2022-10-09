ba5c5412f8e18d1c18a2b54617acb91f6f7a7e0b
Subhadeep354 <myspotsub@gmail.com>
2022-10-09 05:19:06 +0000

    RMX1901: Update remove packages list again * Remove big size packages only.

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -4,12 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := AmbientSensePrebuilt AndroidAutoStubPrebuilt AppDirectedSMSService Camera2 ConnMO DCMO
-LOCAL_OVERRIDES_PACKAGES += DMService GCS GoogleCamera MaestroPrebuilt Maps MicropaperPrebuilt MyVerizonServices
-LOCAL_OVERRIDES_PACKAGES += OBDM_Permissions Ornament SafetyHubPrebuilt SCONE ScribePrebuilt Showcase Snap
-LOCAL_OVERRIDES_PACKAGES += SoundAmplifierPrebuilt SprintDM SprintHM TurboPrebuilt Tycho USCCDM VZWAPNLib
-LOCAL_OVERRIDES_PACKAGES += VzwOmaTrigger WallpapersBReel2020 arcore obdm_stub DevicePolicyPrebuilt RecorderPrebuilt
-LOCAL_OVERRIDES_PACKAGES += Camera2 MusicFX
+LOCAL_OVERRIDES_PACKAGES := RecorderPrebuilt Photos MusicFX GoogleTTS SoundAmplifierPrebuilt GoogleCamera PixelLiveWallpaperPrebuilt
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
