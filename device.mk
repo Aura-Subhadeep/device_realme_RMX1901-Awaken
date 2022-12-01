faa799807ffd4f3b89850e9e182c3a697b10014a
ashim-anwar <ashim.anwar22@gmail.com>
2022-12-01 15:26:46 +0000

    RMX1901: Nuked libhidl

--- device.mk
+++ device.mk
@@ -215,10 +215,9 @@ PRODUCT_PACKAGES += \
 
 # HIDL
 PRODUCT_PACKAGES += \
-    android.hidl.base@1.0 \
-    android.hidl.base@1.0_system \
-    android.hidl.manager@1.0 \
-    android.hidl.manager@1.0_system
+    android.hidl.base@1.0.vendor \
+    libhidltransport.vendor \
+    libhwbinder.vendor
 
 # Input
 PRODUCT_COPY_FILES += \
