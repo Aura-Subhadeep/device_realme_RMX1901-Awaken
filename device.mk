9821217461ad3042fae8c49890b61ecf26e144bf
Aura-Subhadeep <subhadeepadhikari5@gmail.com>
2023-01-07 05:06:21 +0000

    RMX1901: Remove privapp-permissions for gapps * No need to ship it from dt and it's also not updated. * It will be shipped by default from gms.

--- device.mk
+++ device.mk
@@ -182,10 +182,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.freeform_window_management.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.freeform_window_management.xml
 
-# GApps permissions
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/privapp-permissions-gapp.xml:system/product/etc/permissions/privapp-permissions-gapp.xml
-
 # GPS
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/gps/flp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/flp.conf \
